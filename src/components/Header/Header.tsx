a445aade789b91f38a174e87466841a9f59233ec
kalyan-anna <Kalyan.Annamalai@data61.csiro.au>
2020-09-24 07:34:25 +1000

    header fluid

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.scss';
 
 const Header = () => (
   <header className="bg-primary shadow" style={{ height: '70px' }}>
-    <div className="container d-flex justify-content-between">
+    <div className="container-fluid">
       <a href="#" className="navbar-brand d-flex align-items-center">
         <Logo />
         <strong className={`text-white font-weight-bold pl-3 ${styles.title}`}>
