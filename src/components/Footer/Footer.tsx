18ce4725ebbeaff6327adc060a385161fae7e0ed
kalyan-anna <Kalyan.Annamalai@data61.csiro.au>
2020-09-24 15:53:47 +1000

    fix height

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,28 +2,34 @@ import React from 'react';
 import styles from './Footer.module.scss';
 import { Nav } from 'react-bootstrap';
 
+const SocialMedia = () => (
+  <Nav className="d-flex flex-column  d-md-inline-flex flex-md-row">
+    <Nav.Item>
+      <Nav.Link className="text-light" href="https://www.youtube.com/">
+        Youtube
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link className="text-light" href="https://www.facebook.com/">
+        Facebook
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link className="text-light" href="https://twitter.com/">
+        Twitter
+      </Nav.Link>
+    </Nav.Item>
+  </Nav>
+);
+
 const Footer = () => {
   return (
-    <footer className={`bg-secondary text-white ${styles.footer}`}>
-      <div className="container">
-        <span>Stay up to date: </span>
-        <Nav className="d-inline-flex">
-          <Nav.Item>
-            <Nav.Link className="text-light" href="https://www.youtube.com/">
-              Youtube
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link className="text-light" href="https://www.facebook.com/">
-              Facebook
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link className="text-light" href="https://twitter.com/">
-              Twitter
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
+    <footer
+      className={`bg-secondary text-white d-md-flex align-items-center ${styles.footer}`}
+    >
+      <div className="container pt-3 pt-md-0">
+        <strong className="ml-3 ml-md-0">Stay up to date: </strong>
+        <SocialMedia />
         <Nav className="d-inline-flex float-right">
           <Nav.Item>
             <Nav.Link className="text-light" href="/Contact">
